92cf4e81514f7cb81518ad020ffd95c655a6fc15
HuyTran0996 <tranviquanghuy@gmail.com>
2023-11-06 21:04:53 +0700

    feat:add cli command

--- src/App.js
+++ src/App.js
@@ -1,18 +1,30 @@
 import React from "react";
-import { BrowserRouter } from "react-router-dom";
-import Router from "./routes";
-import { AuthProvider } from "./contexts/AuthContext";
-import ThemeProvider from "./contexts/ThemeProvider";
+import Typography from "@mui/material/Typography";
+import { Box, Link } from "@mui/material";
+
+function Copyright() {
+  return (
+    <Typography variant="body2" color="text.secondary" align="center">
+      {"Copyright © "}
+      <Link color="inherit" href="https://www.coderschool.vn">
+        CoderSchool
+      </Link>{" "}
+      {new Date().getFullYear()}
+      {"."}
+    </Typography>
+  );
+}
 
 function App() {
   return (
-    <AuthProvider>
-      <BrowserRouter>
-        <ThemeProvider>
-          <Router />
-        </ThemeProvider>
-      </BrowserRouter>
-    </AuthProvider>
+    <>
+      <Box sx={{ my: 4 }}>
+        <Typography variant="h4" component="h1" gutterBottom align="center">
+          My new React app
+        </Typography>
+        <Copyright />
+      </Box>
+    </>
   );
 }
 
